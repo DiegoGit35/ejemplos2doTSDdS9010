43252cb59e9ff66103e839b1c137feeadfc6fd1c
Martín León Aristiaran <martin.aristiaran@gmail.com>
2023-09-05 21:46:19 -0300

    tateti3

--- lib/tic_tac_toe/tic_tac_toe.dart
+++ lib/tic_tac_toe/tic_tac_toe.dart
@@ -2,7 +2,7 @@ import 'package:bloc/bloc.dart';
 import 'package:ejemplos_2do_soft_9010/tic_tac_toe/tic_tac_toe_state.dart';
 
 class TicTacToeGame extends Cubit<TicTacToeState> {
-  String currentPlayer = '';
+  String currentPlayer = 'X';
   List<String> board = ['', '', '', '', '', '', '', '', ''];
   TicTacToeGame()
       : super(const TicTacToeFirstState(
